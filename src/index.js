927ec8659d34398ae7336a0738281e6da7e629a8
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-08-05 21:18:30 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-  
+import { Bot } from "grammy";
+import { initDB } from "./database.js";
+import { setupStart } from "./handlers/start.js";
+import { setupMenu } from "./handlers/menu.js";
+import { setupMessages } from "./handlers/messages.js";
+
+export default {
+  async fetch(request, env) {
+    const bot = new Bot(env.BOT_TOKEN);
+    
+    // اتصال دیتابیس به context
+    bot.use((ctx, next) => {
+      ctx.env = env;
+      return next();
+    });
+
+    // مقداردهی اولیه دیتابیس (یک بار کافی است، در عمل بهتر است migration جدا داشته باشید)
+    await initDB(env.DB);
+
+    // ثبت هندلرها
+    setupStart(bot);
+    setupMenu(bot);
+    setupMessages(bot);
+
+    // پردازش درخواست
+    return await bot.fetch(request);
+  }
+};
