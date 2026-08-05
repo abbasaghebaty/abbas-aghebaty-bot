9d1afea9bc9b9fabe4a4375fc01c9d03b03d97b5
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-08-05 21:28:46 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,25 +4,26 @@ import { setupStart } from "./handlers/start.js";
 import { setupMenu } from "./handlers/menu.js";
 import { setupMessages } from "./handlers/messages.js";
 
+let dbInitialized = false;
+
 export default {
   async fetch(request, env) {
+    if (!dbInitialized) {
+      await initDB(env.DB);
+      dbInitialized = true;
+    }
+
     const bot = new Bot(env.BOT_TOKEN);
-    
-    // اتصال دیتابیس به context
+
     bot.use((ctx, next) => {
       ctx.env = env;
       return next();
     });
 
-    // مقداردهی اولیه دیتابیس (یک بار کافی است، در عمل بهتر است migration جدا داشته باشید)
-    await initDB(env.DB);
-
-    // ثبت هندلرها
     setupStart(bot);
     setupMenu(bot);
     setupMessages(bot);
 
-    // پردازش درخواست
     return await bot.fetch(request);
   }
 };
