3cf2705f3df03c651fd975e857f69a2f8d6b48c3
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-08-05 21:17:56 +0330

    Update start.js

--- src/handlers/start.js
+++ src/handlers/start.js
@@ -1,1 +1,13 @@
- 
+import { saveUser } from "../database.js";
+import content from "../data/content.json" with { type: "json" };
+import { mainKeyboard } from "../keyboards/mainKeyboard.js";
+
+export function setupStart(bot) {
+  bot.command("start", async (ctx) => {
+    const user = ctx.from;
+    await saveUser(ctx.env.DB, user);
+    await ctx.reply(content.welcome, {
+      reply_markup: mainKeyboard(),
+    });
+  });
+}
