8bb8bb46488c7a53a6e24975d69d9ab1a58a4635
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-08-05 21:17:46 +0330

    Update mainKeyboard.js

--- src/keyboards/mainKeyboard.js
+++ src/keyboards/mainKeyboard.js
@@ -1,1 +1,14 @@
-d
+import { InlineKeyboard } from "grammy";
+import content from "../data/content.json" with { type: "json" };
+
+export function mainKeyboard() {
+  const { buttons } = content;
+  return new InlineKeyboard()
+    .text(buttons.about, "about")
+    .row()
+    .text(buttons.social, "social")
+    .row()
+    .text(buttons.anonymous, "anonymous")
+    .row()
+    .text(buttons.buy, "buy");
+}
