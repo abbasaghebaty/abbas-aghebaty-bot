e808dcf219099e6b6fc2d0d2098045e34fc29f82
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-08-06 05:54:23 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,57 +1,109 @@
-import { Bot } from "grammy";
+import { Bot, webhookCallback } from "grammy";
 import { loadConfig } from "./config.js";
 import { initDB } from "./database.js";
 import { setupStart } from "./handlers/start.js";
 import { setupMenu } from "./handlers/menu.js";
 import { setupMessages } from "./handlers/messages.js";
 
-let bot = null;
+let bot;
 let initialized = false;
+let handleUpdate;
+
+
+function createBot(env) {
+
+  const config = loadConfig(env);
+
+  const bot = new Bot(config.BOT_TOKEN);
+
+
+  bot.use(async (ctx, next) => {
+    ctx.env = env;
+    ctx.config = config;
+    await next();
+  });
+
+
+  setupStart(bot);
+  setupMenu(bot);
+  setupMessages(bot);
+
+
+  bot.catch((err) => {
+    console.error("GRAMMY ERROR:", err);
+  });
+
+
+  return bot;
+}
+
+
 
 export default {
+
   async fetch(request, env) {
-    if (request.method !== "POST") {
-      return new Response("🤖 Abbas Assistant Bot is running.", {
-        status: 200,
-      });
-    }
 
     try {
+
       if (!bot) {
-        const config = loadConfig(env);
 
-        bot = new Bot(config.BOT_TOKEN);
+        bot = createBot(env);
 
-        bot.use(async (ctx, next) => {
-          ctx.env = env;
-          ctx.config = config;
-          await next();
-        });
 
-        if (!initialized && config.DB) {
+        if (env.DB && !initialized) {
           try {
-            await initDB(config.DB);
+            await initDB(env.DB);
             initialized = true;
           } catch (e) {
-            console.error("D1 Init Error:", e);
+            console.error("DB ERROR:", e);
           }
         }
 
-        setupStart(bot);
-        setupMenu(bot);
-        setupMessages(bot);
 
-        bot.catch((err) => {
-          console.error("Bot Error:", err);
-        });
+        handleUpdate = webhookCallback(
+          bot,
+          "cloudflare-mod"
+        );
+
       }
 
-      return await bot.fetch(request);
-    } catch (err) {
-      console.error("Worker Error:", err);
-      return new Response("Internal Server Error", {
-        status: 500,
-      });
+
+      if (
+        request.method === "POST"
+      ) {
+
+        return await handleUpdate(
+          request
+        );
+
+      }
+
+
+      return new Response(
+        "🤖 Abbas Assistant Bot is running.",
+        {
+          status: 200
+        }
+      );
+
+
+    } catch (error) {
+
+      console.error(
+        "WORKER ERROR:",
+        error
+      );
+
+
+      return new Response(
+        "Internal Server Error",
+        {
+          status: 500
+        }
+      );
+
     }
-  },
+
+  }
+
 };
