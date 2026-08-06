3257c91b202434d70e17b01828cb58135b4e3f06
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-08-06 04:53:22 +0330

    Update start.js

--- src/handlers/start.js
+++ src/handlers/start.js
@@ -1,3 +1,4 @@
+// src/handlers/start.js
 import { saveUser } from "../database.js";
 import content from "../data/content.js";
 import { mainKeyboard } from "../keyboards/mainKeyboard.js";
@@ -5,7 +6,10 @@ import { mainKeyboard } from "../keyboards/mainKeyboard.js";
 export function setupStart(bot) {
   bot.command("start", async (ctx) => {
     const user = ctx.from;
-    await saveUser(ctx.env.DB, user);
+    // ذخیره کاربر در دیتابیس (اگر DB موجود باشد)
+    if (ctx.env?.DB) {
+      await saveUser(ctx.env.DB, user);
+    }
     await ctx.reply(content.welcome, {
       reply_markup: mainKeyboard(),
     });
