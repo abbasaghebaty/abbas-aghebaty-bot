33e1f2499a9606af4938bd935b6bb1c2b29368bf
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-08-06 06:30:03 +0330

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,18 @@
 export function loadConfig(env) {
+
   if (!env.BOT_TOKEN) {
-    throw new Error('❌ BOT_TOKEN در متغیرهای محیطی تنظیم نشده است.');
+    throw new Error("❌ BOT_TOKEN تنظیم نشده");
   }
+
+  if (!env.ADMIN_ID) {
+    throw new Error("❌ ADMIN_ID تنظیم نشده");
+  }
+
 
   return {
     BOT_TOKEN: env.BOT_TOKEN,
     DB: env.DB,
+    ADMIN_ID: Number(env.ADMIN_ID)
   };
+
 }
