c1f99a7eee4a6522ece4da3ac674fbc7ca48ee6a
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-08-06 05:07:03 +0330

    Update messages.js

--- src/handlers/messages.js
+++ src/handlers/messages.js
@@ -1,10 +1,7 @@
-// src/handlers/messages.js
 import content from "../data/content.js";
 
 export function setupMessages(bot) {
   bot.on("message:text", async (ctx) => {
-    // نسخه ساده: فعلاً هر پیام متنی را بعنوان پیام ناشناس تلقی می‌کنیم
-    // (در آینده با session مدیریت خواهد شد)
     await ctx.reply(content.anonymous_thanks);
   });
 }
