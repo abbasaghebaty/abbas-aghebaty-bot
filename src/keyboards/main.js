0f20d45384a24b5d61959ca8ef93d6cc7c662df9
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-08-20 20:28:33 +0330

    Update main.js

--- src/keyboards/main.js
+++ src/keyboards/main.js
@@ -7,27 +7,27 @@ export function mainKeyboard() {
     [
       {
         text: BUTTONS.main.socials,
-        ...BUTTON_STYLES.primary,
+        style: BUTTON_STYLES.primary,
       },
       {
         text: BUTTONS.main.skills,
-        ...BUTTON_STYLES.primary,
+        style: BUTTON_STYLES.primary,
       },
     ],
     [
       {
         text: BUTTONS.main.about,
-        ...BUTTON_STYLES.primary,
+        style: BUTTON_STYLES.primary,
       },
       {
         text: BUTTONS.main.buy,
-        ...BUTTON_STYLES.primary,
+        style: BUTTON_STYLES.primary,
       },
     ],
     [
       {
         text: BUTTONS.main.anonymous,
-        ...BUTTON_STYLES.danger,
+        style: BUTTON_STYLES.danger,
       },
     ],
   ]).resized();
