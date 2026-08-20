9a41eb580ffabaf40bec0a40c02c6d2169938cd4
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-08-20 17:48:21 +0330

    Update main.js

--- src/keyboards/main.js
+++ src/keyboards/main.js
@@ -1,15 +1,36 @@
 import { Keyboard } from "grammy";
 import { BUTTONS } from "../config/buttons.js";
+import { BUTTON_STYLES } from "../config/buttonStyles.js";
 
 export function mainKeyboard() {
-  return new Keyboard()
-    .text(BUTTONS.main.socials)
-    .text(BUTTONS.main.skills)
-    .row()
-    .text(BUTTONS.main.about)
-    .text(BUTTONS.main.buy)
-    .row()
-    .text(BUTTONS.main.anonymous)
+  return Keyboard.from([
+    [
+      {
+        text: BUTTONS.main.socials,
+        ...BUTTON_STYLES.primary,
+      },
+      {
+        text: BUTTONS.main.skills,
+        ...BUTTON_STYLES.primary,
+      },
+    ],
+    [
+      {
+        text: BUTTONS.main.about,
+        ...BUTTON_STYLES.primary,
+      },
+      {
+        text: BUTTONS.main.buy,
+        ...BUTTON_STYLES.success,
+      },
+    ],
+    [
+      {
+        text: BUTTONS.main.anonymous,
+        ...BUTTON_STYLES.primary,
+      },
+    ],
+  ])
     .resized()
     .persistent();
 }
