753bf66cd407f8d4544c2a0d50a9c557a947eeb7
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-08-29 14:40:15 +0330

    Update skills.js

--- src/texts/skills.js
+++ src/texts/skills.js
@@ -10,40 +10,28 @@ export const SKILLS_LIST_TEXT = `
 <b>مهارت‌ها</b>
 
 <b>توسعه وب</b>
-<blockquote>
 HTML • CSS • JavaScript
 طراحی رابط‌های کاربری و صفحات واکنش‌گرا
-</blockquote>
 
 <b>ربات‌های تلگرامی</b>
-<blockquote>
 Telegram Bot API • Grammy
 ساخت ربات‌های تعاملی و سیستم‌های کاربردی
-</blockquote>
 
 <b>Backend و Cloud</b>
-<blockquote>
 Cloudflare Workers • D1 • KV • Webhook
 ساخت و مدیریت سرویس‌های Backend سبک و مقیاس‌پذیر
-</blockquote>
 
 <b>ابزارهای تحت وب</b>
-<blockquote>
 ساخت ابزارهای کاربردی و سرویس‌های تحت وب
 با تمرکز روی سادگی، سرعت و تجربه کاربری
-</blockquote>
 
 <b>تولید محتوا</b>
-<blockquote>
 ادیت ویدیو • طراحی کاور و تامنیل
 ساخت و بهینه‌سازی محتوای چندرسانه‌ای
-</blockquote>
 
 <b>UI / UX</b>
-<blockquote>
 طراحی رابط‌های ساده و کاربردی
 با تمرکز روی نظم، خوانایی و تجربه کاربر
-</blockquote>
 
 <i>در حال یادگیری و توسعه مداوم مهارت‌ها...</i>
 `;
