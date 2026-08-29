fe47e32e4cb2833de39e32034401e231cd3deef3
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-08-29 13:58:43 +0330

    Update buttons.js

--- src/config/buttons.js
+++ src/config/buttons.js
@@ -1,22 +1,23 @@
 export const BUTTONS = {
   main: {
-    socials: "🌐 شبکه‌های اجتماعی",
-    skills: "🛠 مهارت‌ها و پروژه‌ها",
-    about: "👤 درباره من",
-    buy: "🛒 خرید فیلترشکن ویتوری",
-    anonymous: "💬 ارسال پیام ناشناس",
+    socials: "شبکه‌های اجتماعی",
+    skills: "مهارت‌ها و پروژه‌ها",
+    about: "درباره من",
+    buy: "خرید ویتوری",
+    anonymous: "پیام ناشناس",
   },
 
   socials: {
-    youtube: "🎬 یوتیوب",
-    instagram: "📷 اینستاگرام",
-    telegram: "📢 تلگرام",
-    back: "🔙",
+    youtube: "یوتیوب",
+    instagram: "اینستاگرام",
+    telegram: "تلگرام",
+    back: "بازگشت",
   },
 
   skills: {
-    list: "🛠 مهارت‌ها",
-    projects: "🚀 پروژه‌ها",
-    back: "🔙",
+    list: "مهارت‌ها",
+    projects: "پروژه‌ها",
+    back: "بازگشت",
+    mainMenu: "منوی اصلی",
   },
 };
