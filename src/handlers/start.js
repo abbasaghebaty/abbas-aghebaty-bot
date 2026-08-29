7123b8895db36785161a83308ddd107e08c713c6
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-08-29 14:00:10 +0330

    Update start.js

--- src/handlers/start.js
+++ src/handlers/start.js
@@ -5,6 +5,9 @@ export function registerStartHandler(bot) {
   bot.command("start", async (ctx) => {
     await ctx.reply(WELCOME_TEXT, {
       parse_mode: "HTML",
+      link_preview_options: {
+        is_disabled: true,
+      },
       reply_markup: mainKeyboard(),
     });
   });
