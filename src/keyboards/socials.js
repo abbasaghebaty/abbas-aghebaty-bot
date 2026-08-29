4ba8569631d25b735bcf0e283e74ecfa844d2136
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-08-29 13:59:28 +0330

    Update socials.js

--- src/keyboards/socials.js
+++ src/keyboards/socials.js
@@ -1,30 +1,54 @@
-import { Keyboard } from "grammy";
+import { InlineKeyboard } from "grammy";
+
 import { BUTTONS } from "../config/buttons.js";
 import { BUTTON_STYLES } from "../config/buttonStyles.js";
 
 export function socialsKeyboard() {
-  return Keyboard.from([
+  return InlineKeyboard.from([
     [
       {
         text: BUTTONS.socials.youtube,
+        callback_data: "social_youtube",
         style: BUTTON_STYLES.primary,
       },
     ],
     [
       {
         text: BUTTONS.socials.instagram,
+        callback_data: "social_instagram",
         style: BUTTON_STYLES.primary,
       },
       {
         text: BUTTONS.socials.telegram,
+        callback_data: "social_telegram",
         style: BUTTON_STYLES.primary,
       },
     ],
     [
       {
-        text: BUTTONS.socials.back,
-        style: BUTTON_STYLES.danger,
+        text: "بازگشت به منوی اصلی",
+        callback_data: "back_main",
+        style: BUTTON_STYLES.primary,
       },
     ],
-  ]).resized();
+  ]);
+}
+
+export function socialDetailKeyboard(url) {
+  return InlineKeyboard.from([
+    [
+      {
+        text: "مشاهده صفحه",
+        url,
+        style: BUTTON_STYLES.success,
+      },
+    ],
+    [
+      {
+        text: "بازگشت",
+        callback_data: "back_socials",
+        style: BUTTON_STYLES.primary,
+      },
+    ],
+  ]);
 }
